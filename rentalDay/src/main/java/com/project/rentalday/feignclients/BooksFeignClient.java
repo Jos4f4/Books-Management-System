0ebfe29e4520e47430e727a88527a4e27e1488e1
Josaf4 <josafa.soares2909@gmail.com>
2024-09-08 14:53:02 -0400

    Feign - Comunication implemented in the two projects

---
+++ rentalDay/src/main/java/com/project/rentalday/feignclients/BooksFeignClient.java
@@ -0,0 +1,18 @@
+package com.project.rentalday.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.project.rentalday.entitities.Books;
+
+@Component
+@FeignClient(name = "Books",  url = "localhost:8001", path = "/books")
+public interface BooksFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Books> findById(@PathVariable Long id);
+}
--- rentalDay/src/main/java/com/project/rentalday/services/PaymentService.java
+++ rentalDay/src/main/java/com/project/rentalday/services/PaymentService.java
@@ -1,33 +1,31 @@
 package com.project.rentalday.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.project.rentalday.entitities.Books;
 import com.project.rentalday.entitities.Payment;
+import com.project.rentalday.feignclients.BooksFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${books.host}")
-	private String booksHost;
+	//@Value("${books.host}")
+	//private String booksHost;
 
-	@Autowired
-	private RestTemplate restTemplate;
+	//@Autowired
+	//private RestTemplate restTemplate;
 	
+	@Autowired
+	private BooksFeignClient booksFeignClient;
 	
 	public Payment getPayment(long BooksId, int days) {
 		//return new Payment("Pai Pobre, Pai rico", "Economia", 10.0, 10);
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+BooksId);
+		//Map<String, String> uriVariables = new HashMap<>();
+		//uriVariables.put("id", ""+BooksId);
 		
-		Books books = restTemplate.getForObject(booksHost + "/books/{id}", Books.class, uriVariables);
+		Books books = booksFeignClient.findById(BooksId).getBody();
 		return new Payment(books.getName(), books.getGender(), books.getRentalday(), days);
 	}
 }
