245693497ea0047ea3798b7b55a7523ae07235ab
Josaf4 <josafa.soares2909@gmail.com>
2024-09-11 02:17:21 -0400

    Implementation Eureka Server and Eureka Client - Sucessfull

---
+++ books-EurekaServer/src/main/java/com/project/booksEurekaServer/BooksEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.project.booksEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BooksEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksEurekaServerApplication.class, args);
+	}
+
+}
---
+++ books-EurekaServer/src/test/java/com/project/booksEurekaServer/BooksEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booksEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
