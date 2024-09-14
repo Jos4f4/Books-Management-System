78af918cc4492964485c595b54b569f042482c1c
Josaf4 <josafa.soares2909@gmail.com>
2024-09-14 01:29:05 -0400

    Project books-api-gateway-zuul created

---
+++ books-api-gateway-zuul/src/main/java/com/project/booksapigatewayzuul/BooksApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.project.booksapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ books-api-gateway-zuul/src/test/java/com/project/booksapigatewayzuul/BooksApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booksapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
